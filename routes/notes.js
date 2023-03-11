c8c4a961a9249e8651ff2dc6d2449bd3678af935
miamauro <miiamauro@outlook.com>
2023-03-11 17:28:27 -0600

    created helpers folder to write logic for reading and appending notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,9 @@
 const notes = require("express").Router();
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+//const noteId =
 
 notes.get("/", (req, res) =>
-  readFromFile("./db/db.json").then((data = res.json(JSON.parse(data))))
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 );
 
 notes.post("/", (req, res) => {
