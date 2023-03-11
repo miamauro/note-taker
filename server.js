180cf26698591690c8f10b07a236d002e0f41671
miamauro <miiamauro@outlook.com>
2023-03-11 14:53:46 -0600

    added get routes for homepage and notes.html

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ const express = require("express");
 const path = require("path");
 const api = require("./public/assets/js/index.js");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-const PORT = 3001;
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
+
+//GET route from homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+//GET route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//GET route for wildcard
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
